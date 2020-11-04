aeb47bd5767e2f6a5a8ffd19cc2566d9ae4ecf50
Arthur Conti <65549367+arthurgconti@users.noreply.github.com>
2020-11-03 22:30:00 -0300

    Cliente, acho que tudo ta aqui

--- clientes.h
+++ clientes.h
@@ -3,15 +3,16 @@
 #include "types.h"
 
 
-
 void CadastrarCliente(Cliente *, int *);
-void AlterarCliente(Cliente *, int *);
-void ExcluirCliente(Cliente *, int *, int *);
-void ConsultarCliente(Cliente *, int *);
+void AlterarCliente(Cliente *, int *,int *);
+void ExcluirCliente(Cliente *, int *, int *, int *);
+void TelaAlterar(Cliente *);
+void ConsultarCliente(Cliente *, int *, int *);
 void LimparEspacoVazio(Cliente *);
 void CalcularMensalidade(Cliente *, int *);
-int PesquisarCodigo(Cliente *, int);
+int PesquisarCodigo(Cliente *, int, int);
 int PesquisarIndiceVazio(Cliente *);
 int ExisteCodigo(Cliente *, int, int);
+int MenuAlterarCliente();
 
 #endif
